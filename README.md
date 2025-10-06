c7696518e80849cca63ed0429597ba9193c382aa
deanissa <deanissa604@gmail.com>
2025-10-06 15:16:30 +0700

    menerapkan date and pickers + dokumentasi

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 A new Flutter project.
 
-![Screenshot hello_world](images/03.png)
-![Screenshot hello_world](images/04.png)
+![Screenshot hello_world](images/03(2).png)
+![Screenshot hello_world](images/03(3).png)
+![Screenshot hello_world](images/03(4).png)
+![Screenshot hello_world](images/03(5).png)
--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,19 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
-   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: MyLayout(),
-      ),
-    );
-  }
-}
-
-class MyLayout extends StatelessWidget {
-  const MyLayout({Key? key}) : super(key: key);
-
-  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ElevatedButton(
-        child: const Text('Show alert'),
-        onPressed: () {
-          showAlertDialog(context);
-        },
-      ),
+    return const MaterialApp(
+      title: 'Contoh Date Picker',
+      home: MyHomePage(title: 'Contoh Date Picker'),
     );
   }
-}
-
-showAlertDialog(BuildContext context) {
-  // set up the button
-  Widget okButton = TextButton(
-    child: const Text("OK"),
-    onPressed: () {
-      Navigator.pop(context);
-    },
-  );
-
-  // set up the AlertDialog
-  AlertDialog alert = AlertDialog(
-    title: const Text("My title"),
-    content: const Text("This is my message."),
-    actions: [
-      okButton,
-    ],
-  );
-
-  // show the dialog
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return alert;
-    },
-  );
 }
 
 class MyHomePage extends StatefulWidget {
@@ -72,45 +26,48 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // Variable/State untuk mengambil tanggal
+  DateTime selectedDate = DateTime.now();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  //  Initial SelectDate FLutter
+  Future<void> _selectDate(BuildContext context) async {
+    // Initial DateTime FIinal Picked
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2015, 8),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: Text(widget.title)),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Text("${selectedDate.toLocal()}".split(' ')[0]),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () => {
+                _selectDate(context),
+                // ignore: avoid_print
+                print(
+                  selectedDate.day + selectedDate.month + selectedDate.year,
+                ),
+              },
+              child: const Text('Pilih Tanggal'),
             ),
           ],
         ),
       ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          height: 50.0,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment Counter',
-        child: const Icon(Icons.add),
-      ), 
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
